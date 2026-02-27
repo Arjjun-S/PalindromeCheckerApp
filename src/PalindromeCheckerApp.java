92a0b66ee5e3605eb2709eddaedbd1b609772732
Arjjun-S <suresharjjun@gmail.com>
2026-02-27 09:55:57 +0530

    uc5: stack based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,7 +4,7 @@ public class PalindromeCheckerApp
     public static void main(String[] args)
     {
         System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version 1.0");
+        System.out.println("Version 1.5");
         System.out.println("System initialized Sucessfully");
         System.out.println("Executing UC5: Stack-Based Palindrome Check");
         String input = "radar";
