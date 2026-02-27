327b7a7898289bb1e6647ca15b36717015b20222
Arjjun-S <suresharjjun@gmail.com>
2026-02-27 09:55:31 +0530

    uc5: stack based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,65 +3,9 @@ public class PalindromeCheckerApp
 {
     public static void main(String[] args)
     {
-        PalindromeCheckerApp t=new PalindromeCheckerApp();
-        t.Usecase1();
-        t.Usecase2();
-        t.Usecase3();
-    }
-    static void Usecase1(){
         System.out.println("Welcome to Palindrome Checker Management System");
         System.out.println("Version 1.0");
         System.out.println("System initialized Sucessfully");
-    }
-    static void Usecase2()
-    {
-        String input="madam";
-        char[] test= input.toCharArray();
-        int n=input.length()-1;
-        int j=0;
-        for(int i=0;i<input.length()/2;i++)
-        {
-            if(test[i]!=(test[n])){
-                System.out.println("The String is not palindrome");
-                j=1;
-                break;
-            }
-            n--;
-        }
-        if(j==0){
-            System.out.println("The String is palindrome");
-        }
-    }
-    static void Usecase3() {
-        String original = "racecar";
-        String reversed = "";
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
-        }
-            if (original.equals(reversed)) {
-                System.out.println("The String is a palindrome");
-            } else {
-                System.out.println("The String is not a palindrome");
-            }
-    }
-    static void Usecase4() {
-        String input = "level";
-        char[] charArray = input.toCharArray();
-        int start = 0;
-        int end = charArray.length - 1;
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (charArray[start] != charArray[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-        System.out.println("UC4 Result: Is '" + input + "' a palindrome? " + isPalindrome);
-    }
-    static void Usecase5() {
         System.out.println("Executing UC5: Stack-Based Palindrome Check");
         String input = "radar";
         Stack<Character> stack = new Stack<Character>();
